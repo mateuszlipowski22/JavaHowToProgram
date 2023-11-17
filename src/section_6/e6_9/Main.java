44963195e1c41cd95f820cf3ea157c57fb3b1271
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-11-17 21:57:30 +0100

    task 6.9

---
+++ src/section_6/e6_9/Main.java
@@ -0,0 +1,20 @@
+package section_6.e6_9;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Wprowadz liczbe :");
+            double number = scanner.nextDouble();
+            System.out.printf("Liczba przed zaokragleniem : %f %n", number);
+            System.out.printf("Liczba po zaokragleniu : %d %n", roundToClosestInt(number));
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+    public static int roundToClosestInt(double number){
+        return (int) Math.floor(number+0.5);
+    }
+}
