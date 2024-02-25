c12ebb967bb309d57f7bdf67cac582c3420f691f
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-02-25 22:42:33 +0100

    example 14.17

---
+++ src/section_14/section_14_17/OtherCharMethods.java
@@ -0,0 +1,16 @@
+package section_14.section_14_17;
+
+public class OtherCharMethods {
+    public static void main(String[] args) {
+        Character c1 = 'A';
+        Character c2 = 'a';
+
+        System.out.printf("c1 - %s%nc2 - %s%n", c1.charValue(),c2.toString());
+
+        if(c1.equals(c2)){
+            System.out.println("c1 i c2 sa równe");
+        }else {
+            System.out.println("c1 i c2 sa rózne");
+        }
+    }
+}
