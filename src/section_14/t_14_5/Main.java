243a08d48461ed07c878d7afb6d86c4fb3a0b2e6
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-03-01 23:41:14 +0100

    task 14.5

---
+++ src/section_14/t_14_5/Main.java
@@ -0,0 +1,22 @@
+package section_14.t_14_5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Wprowadz pierwszy tekst:");
+            String first = scanner.nextLine();
+
+            System.out.println("Wprowadz drugi tekst:");
+            String second = scanner.nextLine();
+
+            System.out.printf("first.compareTo(second) = %d", first.compareTo(second));
+
+        }catch (Exception e){
+            System.out.println("Bład podczas wprowadzania danych przez użytownika");
+        }
+    }
+}
