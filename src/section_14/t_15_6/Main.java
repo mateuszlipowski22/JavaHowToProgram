91fd85a01d61ea17abf61d6896d8eee1038b31f8
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-03-02 23:19:41 +0100

    task 14.6

---
+++ src/section_14/t_15_6/Main.java
@@ -0,0 +1,28 @@
+package section_14.t_15_6;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Wprowadz pierwszy tekst:");
+            String first = scanner.nextLine();
+
+            System.out.println("Wprowadz drugi tekst:");
+            String second = scanner.nextLine();
+
+            System.out.println("Wprowadz liczbe znaków do porównania:");
+            int numberOfChars = scanner.nextInt();
+
+            System.out.println("Wprowadz indeks początkowy:");
+            int startIndex = scanner.nextInt();
+
+            System.out.printf("first.regionMatches(second) = %b", first.regionMatches(true, startIndex,second,startIndex,numberOfChars));
+
+        }catch (Exception e){
+            System.out.println("Bład podczas wprowadzania danych przez użytownika");
+        }
+    }
+}
