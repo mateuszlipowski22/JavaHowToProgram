5170d025000a48af5794d8b6a6cab4335c848f32
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-03-14 23:10:01 +0100

    example 16.2

---
+++ src/section_16/example_16_2/CollectionTest.java
@@ -0,0 +1,52 @@
+package section_16.example_16_2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollectionTest {
+    public static void main(String[] args) {
+        String[] colors = {"MAGENTA", "CZERWONY","BIAŁY", "NIEBIESKI"};
+
+        List<String> list = new ArrayList<>();
+
+        for(String color : colors){
+            list.add(color);
+        }
+
+        String[] removeColors = {"CZERWONY","BIAŁY", "NIEBIESKI"};
+
+        List<String> removeList = new ArrayList<>();
+
+        for(String color : removeColors){
+            removeList.add(color);
+        }
+
+        System.out.println("ArrayList: ");
+
+        for(int count = 0 ; count < list.size(); count++){
+            System.out.printf("%s ",list.get(count));
+        }
+
+        removeColors(list, removeList);
+
+        System.out.printf("%nArrayList po wywołaniu metody removeColors(): %n");
+
+        for(int count = 0 ; count < list.size(); count++){
+            System.out.printf("%s ",list.get(count));
+        }
+    }
+
+    private static void removeColors(Collection<String> collection1, Collection<String> collection2) {
+
+        Iterator<String> iterator = collection1.iterator();
+
+        while (iterator.hasNext()){
+            if(collection2.contains(iterator.next())){
+                iterator.remove();
+            }
+        }
+
+    }
+}
