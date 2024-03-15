c90e433468211f436d7d3776c3d2b0c9fc6b075b
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-03-15 17:16:38 +0100

    example 16.9

---
+++ src/section_16/e_16_9/Card.java
@@ -0,0 +1,19 @@
+package org.programowanie_w_java.section_16.e_16_9;
+
+public class Card {
+    public enum Face {As, Dwókja, Trójka, Czwórka, Piątka, Szóstka,
+            Siódemka, Ósemka, Dziewiątka, Dziesiątka, Walet, Dama, Król};
+    public enum Suit  {trefl, karo, kier, pik};
+    private final Face face;
+    private final Suit suit;
+
+    public Card(Face face, Suit suit) {
+        this.face = face;
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", face, suit);
+    }
+}
