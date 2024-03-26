ea228ec1e945332d1f1f794d78d691b7eaf717ff
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-03-26 23:54:38 +0100

    exercise 18.3

---
+++ src/section_18/e_18_3/FactorialCalculator.java
@@ -0,0 +1,17 @@
+package section_18.e_18_3;
+
+public class FactorialCalculator {
+    public static long factorial(long number) {
+        if(number<=1){
+            return 1;
+        }else {
+            return number*factorial(number-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int counter = 0; counter <=21 ; counter++){
+            System.out.printf("%d! = %d%n", counter, factorial(counter));
+        }
+    }
+}
