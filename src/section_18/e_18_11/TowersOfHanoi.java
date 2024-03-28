c214e6e9dc65f6159116407d7a96f73db7451a8f
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-03-28 22:40:00 +0100

    exercise 18.11

---
+++ src/section_18/e_18_11/TowersOfHanoi.java
@@ -0,0 +1,25 @@
+package section_18.e_18_11;
+
+public class TowersOfHanoi {
+    public static void solveTowers(int disks, int sourcePeg, int destinationPeg, int tempPeg){
+
+        if(disks == 1){
+            System.out.printf("%n%d --> %d", sourcePeg, destinationPeg);
+            return;
+        }
+
+        solveTowers(disks-1,sourcePeg,tempPeg,destinationPeg);
+        System.out.printf("%n%d --> %d", sourcePeg, destinationPeg);
+
+        solveTowers(disks-1,tempPeg,destinationPeg,sourcePeg);
+    }
+
+    public static void main(String[] args) {
+        int startPeg = 1;
+        int endPeg = 3;
+        int tempPeg = 2;
+        int totalDisks = 3;
+
+        solveTowers(totalDisks,startPeg,endPeg,tempPeg);
+    }
+}
