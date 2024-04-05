72b4af811be8564eaf42dbeeb392a87f59ed360e
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-04-05 23:22:53 +0200

    task 19.5

--- src/section_19/t_19_5/BubbleSort.java
+++ src/section_19/t_19_5/BubbleSort.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void bubbleSort(int[] data) {
-        for(int i = 0 ; i < data.length-2;i++){
-            for(int j = i+1; j< data.length-2 ; j++){
+        for(int i = 0 ; i < data.length-1;i++){
+            for(int j = i+1; j<data.length ; j++){
                 if(data[i]>data[j]){
                     swap(data,i,j);
+                    System.out.printf("%d.%d : %s%n%n",i+1,j,Arrays.toString(data));
                 }
             }
+            System.out.printf("%d : %s%n%n",i+1, Arrays.toString(data));
         }
     }
 
