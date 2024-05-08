d043a0444f61cf550b3abe8673463e0a9d07a829
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-05-08 23:23:19 +0200

    task 21.24

---
+++ src/section_21/t_21_24/TreeTest.java
@@ -0,0 +1,29 @@
+package section_21.t_21_24;
+
+import java.security.SecureRandom;
+
+public class TreeTest {
+    public static void main(String[] args) {
+        Tree<Integer> tree = new Tree<Integer>();
+        SecureRandom random = new SecureRandom();
+
+        System.out.println("Wstawianie następujących wartości: ");
+        random.ints(10,0,100)
+                .forEach(element -> {
+                    System.out.printf("%d ", element);
+                    tree.insertNode(element);
+                });
+
+        System.out.printf("%n%nPrzejście wzdłużne%n");
+        tree.preorderTraversal();
+
+        System.out.printf("%n%nPrzejście poprzeczne%n");
+        tree.inorderTraversal();
+
+        System.out.printf("%n%nPrzejście wsteczne%n");
+        tree.postorderTraversal();
+
+        System.out.printf("%n%nPrzejście poziomami%n");
+        tree.levelOrder();
+    }
+}
